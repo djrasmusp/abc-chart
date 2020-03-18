d1777c5375bb1e44af0d55584722e4396889e1a8
Rasmus Pedersen <hello@rasmusp.com>
2020-03-18 17:03:24 +0100

    env test

--- abc-chart-parser.php
+++ abc-chart-parser.php
@@ -26,4 +26,33 @@ $dotenv->load( plugin_dir_path( __FILE__ ) . '/.env' );
 require plugin_dir_path( __FILE__ ) . '/requires/pdf-to-html.php';
 require plugin_dir_path( __FILE__ ) . '/requires/parse-html.php';
 
+function pdf2html( $data ) {
+	$url  = 'https://cameleonpdf.p.rapidapi.com/pdf2html/.';
+	$host = 'cameleonpdf.p.rapidapi.com';
+	$key  = $_ENV( 'API_KEY' );
+
+	$response = wp_remote_post(
+		$url,
+		array(
+			'method'  => 'POST',
+			'headers' => array(
+				'x-rapidapi-host: ' . $host,
+				'x-rapidapi-key: ' . $key,
+				'content-type: application/pdf'
+			),
+			'body'    => array(
+				'PDF' => $data
+			)
+		)
+	);
+
+	if ( is_wp_error( $response ) ) {
+		$error_message = $response->get_error_message();
+
+		return 'Something went wrong: ' . $error_message;
+	}
+
+	return $response;
+}
+
 pdf2html( $_POST );
