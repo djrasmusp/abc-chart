e30ffdade739d386182af0d3caf2ead8ac322af0
Rasmus Pedersen <hello@rasmusp.com>
2020-03-19 00:47:43 +0100

    sql fix

--- abc-chart-parser.php
+++ abc-chart-parser.php
@@ -25,21 +25,25 @@ require plugin_dir_path( __FILE__ ) . '/requires/abc-settings.php';
 
 register_activation_hook( __FILE__, 'abc_chart_parser_activate' );
 function abc_chart_parser_activate() {
-
 	global $wpdb;
 	$charset_collate = $wpdb->get_charset_collate();
 	$table_name      = $wpdb->prefix . 'abc_chart_parser';
 
-	$sql = "CREATE TABLE ". $table_name ." (
+	$sql = "CREATE TABLE " . $table_name . " (
 		id mediumint(9) NOT NULL AUTO_INCREMENT,
 		title text,
 		type smallint(5) NOT NULL,
 		chart longtext,
 		UNIQUE KEY id (id)
-	) ". $charset_collate .";";
+	) " . $charset_collate . ";";
 
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta($sql);
+	$result = dbDelta( $sql );
+	ob_start();
+	print_r( $result );
+	file_put_contents( plugin_dir_path( __FILE__ ) . '/dbdebug.txt',
+	                   ob_get_contents() );
+	ob_end_clean();
 }
 
 // pdf2html( $_POST );
